47cef2e4fda58e4bf1727a9f241c9c66140f8099
Virtual Self <a01722401@tec.mx>
2026-04-10 01:51:39 +0100

    change to IQM optimization

--- src/optimization.py
+++ src/optimization.py
@@ -17,6 +17,12 @@ opt_log = manager.get_logger(name = "Optuna", file_name = "optimization.log", to
 
 optuna.logging.set_verbosity(optuna.logging.ERROR)
 
+def get_iqm(sample):
+    sample = np.array(sample)
+    q1 = np.quantile(sample, 0.25)
+    q3 = np.quantile(sample, 0.75)
+    return np.mean(sample[(sample >= q1) & (sample <= q3)])
+
 def objective(trial: Trial, env, algorithm_func, n_seeds=5):
     # Suggest parameters
     params = {
@@ -25,7 +31,7 @@ def objective(trial: Trial, env, algorithm_func, n_seeds=5):
         "epsilon": 1.0,
         "epsilon_decay": trial.suggest_float("epsilon_decay", 0.9, 0.9999),
         "epsilon_min": 0.01,
-        "n_episodes": 100
+        "n_episodes": 400
     }
     
     seed_scores = []
@@ -37,42 +43,38 @@ def objective(trial: Trial, env, algorithm_func, n_seeds=5):
         
         seed_auc = np.sum(rewards)
         seed_scores.append(seed_auc)
-        
-        # if trial.should_prune():
-        #     raise optuna.exceptions.TrialPruned()
 
         opt_log.info(f"Trial {trial.number} | Seed {seed} | Ended | Score: {params}")
         
-    mu = np.mean(seed_scores)
-    sigma = np.std(seed_scores)
-    
-    opt_log.info(f"Trial {trial.number} | Scores: ({mu}, {sigma})")
+    iqm = get_iqm(seed_scores)
+    opt_log.info(f"Trial {trial.number} | Score: {iqm}")
 
-    return mu, sigma
+    return iqm
 
-def param_opt_pipeline(algorithm, env, n_trials = 100):
-
-    # pruner = optuna.pruners.MedianPruner(n_startup_trials = 5, n_warmup_steps = 1)
+def param_opt_pipeline(algorithm, env, n_trials = 64):
 
     study = optuna.create_study(
         storage = "sqlite:///param_opt.sqlite3",
         study_name = algorithm.__name__,
         load_if_exists = True,
-        directions=["maximize", "minimize"],
+        direction = "maximize",
+        pruner = optuna.pruners.MedianPruner()
     )
 
     with tqdm(total = n_trials, desc = f"Optimizing {algorithm.__name__}") as pbar:
         
         def callback(study, trial):
             pbar.update(1)
-            # pbar.set_postfix({"Best Reward": f"{study.best_value:.2f}"})
 
         study.optimize(
-            # Pass show_progress=False to the algorithm here
             lambda trial: objective(trial, env, algorithm), 
             n_trials = n_trials,
-            n_jobs = -1,
+            # n_jobs = -1,
             callbacks = [callback]
         )
 
-    return study.best_trials
+        # n_jobs helps with doing multiple trials at once,
+        # but sacrifices reproducibility. Therefore, while slower,
+        # one job at a time will be done.
+
+    return study.best_trial
